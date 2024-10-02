e33d41d410e86fe9032b648131d271b622ddbd96
raghveerdeol <deolraghveer5@gmail.com>
2024-10-02 11:10:45 +0200

    seeded profume table

--- database/seeders/ProfumeSeeder.php
+++ database/seeders/ProfumeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class ProfumeSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class ProfumeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        for ($i=0; $i < 10; $i++) {
+            DB::table('profumes')->insert([
+                'name' => $faker->name(),
+                'description' => $faker->text(200),
+                'price' => $faker->randomFloat(2, 1, 1000),
+                'quantity' => $faker->randomNumber(4, false),
+                'brand' => $faker->name(),
+                'image' => $faker->image(null, 400, 600, 'profumes'),
+                'sponsored' => $faker->boolean()
+            ]);
+        }
     }
 }
