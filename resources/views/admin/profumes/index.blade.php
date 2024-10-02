c564bcc06a4ad91201628012e74c2cbbbead9096
raghveerdeol <deolraghveer5@gmail.com>
2024-10-02 19:51:55 +0200

    changed list in a table and added button for a show

--- resources/views/admin/profumes/index.blade.php
+++ resources/views/admin/profumes/index.blade.php
@@ -2,11 +2,37 @@
 
 @section('content')
 
-    <ul>
-        @foreach ($profumes as $profume)
-        <li>
-            {{$profume->name}}
-        </li>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Price</th>
+                <th scope="col">is sponsored?</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($profumes as $profume)
+            <tr>
+                <td>{{ $profume->name }}</td>
+                <td>{{ $profume->brand }}</td>
+                <td>{{ $profume->quantity }}</td>
+                <td>{{ $profume->price }}£</td>
+                <td>
+                    @if ($profume->sponsored === 1)
+                        Sponsored
+                    @else
+                        Not sponsored
+                    @endif
+                </td>
+                <td>
+                    <a href="{{ route('admin.profumes.show', compact('profume')) }}" type="button" class="btn btn-primary">Show</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 @endsection
