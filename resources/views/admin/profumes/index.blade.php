79f4085fd536a3b06e9e80cc514152f3e2e2225f
raghveerdeol <deolraghveer5@gmail.com>
2024-10-02 20:14:01 +0200

    added card in a show

--- resources/views/admin/profumes/index.blade.php
+++ resources/views/admin/profumes/index.blade.php
@@ -1,38 +1,43 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('content')
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Brand</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Price</th>
-                <th scope="col">is sponsored?</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($profumes as $profume)
-            <tr>
-                <td>{{ $profume->name }}</td>
-                <td>{{ $profume->brand }}</td>
-                <td>{{ $profume->quantity }}</td>
-                <td>{{ $profume->price }}£</td>
-                <td>
-                    @if ($profume->sponsored === 1)
-                        Sponsored
-                    @else
-                        Not sponsored
-                    @endif
-                </td>
-                <td>
-                    <a href="{{ route('admin.profumes.show', compact('profume')) }}" type="button" class="btn btn-primary">Show</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Brand</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">is sponsored?</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($profumes as $profume)
+                        <tr>
+                            <td>{{ $profume->name }}</td>
+                            <td>{{ $profume->brand }}</td>
+                            <td>{{ $profume->quantity }}</td>
+                            <td>{{ $profume->price }}£</td>
+                            <td>
+                                @if ($profume->sponsored === 1)
+                                    Sponsored
+                                @else
+                                    Not sponsored
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{ route('admin.profumes.show', compact('profume')) }}" type="button" class="btn btn-primary">Show</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
--- resources/views/admin/profumes/show.blade.php
+++ resources/views/admin/profumes/show.blade.php
@@ -5,5 +5,23 @@
 @endsection
 
 @section('content')
-    {{ $profume->name }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-8" style="width: 18rem;">
+                <img src="{{ asset('storage/'.$profume->image) }}" class="card-img-top" alt="{{ $profume->name }} image">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $profume->name }}</h5>
+                    <h6><em>{{ $profume->brand }}</em></h6>
+                    <p class="card-text">{{ $profume->description }}</p>
+                    <p><strong>Quantity: {{ $profume->quantity }}</strong></p>
+                    <p><strong>Price: {{ $profume->price }}</strong></p>
+
+                    @if ($profume->sponsored === 1)
+                        <span class="badge text-bg-success">Sponsored</span>
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
